a370ad7cfcb63e5682c27cbe4f8d30c383ecc844
me001416 <me001416@gmail.com>
2017-11-28 14:18:51 +0800

    Version 1.0.20

---
+++ YoungHero/NpcData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoungHero
+{
+    public class NpcData
+    {
+        private bool updated;
+
+        private int npcId;
+        public int NpcId
+        {
+            get { return npcId; }
+            set
+            {
+                npcId = value;
+            }
+        }
+    }
+}
