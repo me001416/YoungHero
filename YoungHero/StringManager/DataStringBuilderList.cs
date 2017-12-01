0883128bde87ea5d99067da41b7de35d5cd50252
me001416 <me001416@gmail.com>
2017-12-01 11:55:23 +0800

    Version 1.0.43

--- YoungHero/StringManager/DataStringBuilderList.cs
+++ YoungHero/StringManager/DataStringBuilderList.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace YoungHero.StringManager
 {
-    class DataStringBuilderList
+    public class DataStringBuilderList
     {
+        public List<DataStringBuilder> dsList { get; private set; }
+
+        public DataStringBuilderList()
+        {
+            dsList = new List<DataStringBuilder>();
+        }
+
+        public void Add(StringBuilder SrcSb)
+        {
+            if (dsList.Count == 0)
+            {
+                this.New();
+            }
+
+            dsList[dsList.Count - 1].Add(SrcSb);
+        }
+
+        public void Add(StringBuilder SrcSb, int SrcIndex)
+        {
+            dsList[SrcIndex].Add(SrcSb);
+        }
+
+        public void New()
+        {
+            dsList.Add(new DataStringBuilder(dsList.Count - 1));
+        }
+
+        public void New(int SrcIndex)
+        {
+            dsList.Add(new DataStringBuilder(SrcIndex));
+        }
+    }
+
+    public class DataStringBuilder
+    {
+        public List<StringBuilder> SbList { get; private set; }
+        int Index;
+
+        public DataStringBuilder()
+        {
+            Index = 0;
+            SbList = new List<StringBuilder>();
+        }
+
+        public DataStringBuilder(int SrcIndex)
+        {
+            Index = SrcIndex;
+            SbList = new List<StringBuilder>();
+        }
+
+        public void Add(StringBuilder SrcSb)
+        {
+            SbList.Add(SrcSb);
+        }
     }
 }
--- YoungHero/StringManager/DataStringList.cs
+++ YoungHero/StringManager/DataStringList.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace YoungHero.StringManager
 {
-    class DataStringList
+    public class DataStringList
     {
+        public List<DataString> dsList { get; private set; }
+
+        public DataStringList()
+        {
+            dsList = new List<DataString>();
+        }
+
+        public void Add(string SrcSb)
+        {
+            if (dsList.Count == 0)
+            {
+                this.New();
+            }
+
+            dsList[dsList.Count - 1].Add(SrcSb);
+        }
+
+        public void Add(string SrcSb, int SrcIndex)
+        {
+            dsList[SrcIndex].Add(SrcSb);
+        }
+
+        public void New()
+        {
+            dsList.Add(new DataString(dsList.Count - 1));
+        }
+
+        public void New(int SrcIndex)
+        {
+            dsList.Add(new DataString(SrcIndex));
+        }
+    }
+
+    public class DataString
+    {
+        public List<string> SbList { get; private set; }
+        int Index;
+
+        public DataString()
+        {
+            Index = 0;
+            SbList = new List<string>();
+        }
+
+        public DataString(int SrcIndex)
+        {
+            Index = SrcIndex;
+            SbList = new List<string>();
+        }
+
+        public void Add(string SrcSb)
+        {
+            SbList.Add(SrcSb);
+        }
     }
 }
