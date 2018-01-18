89b8289839be2c21f7b51fe56d2b1526f4daaa92
me001416 <me001416@gmail.com>
2018-01-18 17:05:06 +0800

    Version 1.0.104

--- YoungHero/MainForm.cs
+++ YoungHero/MainForm.cs
@@ -3,31 +3,52 @@ using System.Windows.Forms;
 
 namespace YoungHero
 {
+    /// <summary>
+    /// 起始視窗
+    /// </summary>
     public partial class MainForm : Form
     {
+        /// <summary>
+        /// Main Constructor Function
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// 前往修改頁面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             LoadSaveFile formLSF = new LoadSaveFile();
-            formLSF.Top = this.Top;
-            formLSF.Left = this.Left + (this.Width / 3);
+            formLSF.Top = Top;
+            formLSF.Left = Left + (Width / 3);
             formLSF.ShowDialog();
         }
 
+        /// <summary>
+        /// 離開
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button2_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
+        /// <summary>
+        /// 針對遊戲資料製做 Json File
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button3_Click(object sender, EventArgs e)
         {
             DataMaker dmk = new DataMaker();
-            dmk.Top = this.Top;
-            dmk.Left = this.Left;
+            dmk.Top = Top;
+            dmk.Left = Left;
             dmk.ShowDialog();
         }
     }
