6d5a2096e2946579def0e5de526a59db64c6621c
me001416 <me001416@gmail.com>
2018-01-23 16:00:55 +0800

    Version 1.0.109

--- YoungHero/StringManager/DataStringBuilderList.cs
+++ YoungHero/StringManager/DataStringBuilderList.cs
@@ -1,35 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace YoungHero.StringManager
 {
+    /// <summary>
+    /// 管理 List StringBuild 的 Class
+    /// </summary>
     public class DataStringBuilderList
     {
+        /// <summary>
+        /// List StringBuild，提供使用者存取字串
+        /// </summary>
         public List<DataStringBuilder> dsList { get; private set; }
 
+        /// <summary>
+        /// Constructor Function
+        /// </summary>
         public DataStringBuilderList()
         {
             dsList = new List<DataStringBuilder>();
         }
 
+        /// <summary>
+        /// 新增 StringBuilder 到 DataStringBuilder 裡面
+        /// </summary>
+        /// <param name="SrcSb"></param>
         public void Add(StringBuilder SrcSb)
         {
             if (dsList.Count == 0)
             {
-                this.New();
+                New();
             }
 
             dsList[dsList.Count - 1].Add(SrcSb);
         }
 
+        /// <summary>
+        /// 新增 StringBuilder 透過特殊的 Index
+        /// </summary>
+        /// <param name="SrcSb"></param>
+        /// <param name="SrcIndex"></param>
         public void Add(StringBuilder SrcSb, int SrcIndex)
         {
             dsList[SrcIndex].Add(SrcSb);
         }
 
+        /// <summary>
+        /// 新增 DataStringBuilder 到 List
+        /// </summary>
         public void New()
         {
             dsList.Add(new DataStringBuilder(dsList.Count - 1));
--- YoungHero/StringManager/DataStringList.cs
+++ YoungHero/StringManager/DataStringList.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace YoungHero.StringManager
 {
@@ -19,7 +15,7 @@ namespace YoungHero.StringManager
         {
             if (dsList.Count == 0)
             {
-                this.New();
+                New();
             }
 
             dsList[dsList.Count - 1].Add(SrcSb);
